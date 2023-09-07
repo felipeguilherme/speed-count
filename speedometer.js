bd051c808918aaef22c63fc56405038771659f65
felipeguilherme <felipe@felipeguilherme.com>
2023-09-07 02:07:00 -0300

    feat: fist commit - logic js

---
+++ speedometer.js
@@ -0,0 +1,39 @@
+const speedElement = document.querySelector('#speed');
+const startBtn = document.querySelector('#start');
+const stopBtn = document.querySelector('#stop');
+
+let watchId = null;
+
+
+
+startBtn.addEventListener('click', () => {
+
+    if(watchId )
+        return
+    
+
+    function handleSuccess(position) {
+        speedElement.innerText = position.coords.speed ? (position.coords.speed * 3.6).toFixed(1) : 0;
+    }
+
+    function handleError(error) {
+        console.error(error.msg);   
+    }
+
+    const options = {enableHighAccuracy: true};
+
+    watchId = navigator.geolocation.watchPosition(handleSuccess, handleError, options);
+    startBtn.classList.add('d-none');
+    stopBtn.classList.remove('d-none');
+
+})
+;
+stopBtn.addEventListener('click', () => {
+        if(!watchId)
+        return
+        navigator.geolocation.clearWatch(watchId);
+        watchId = null;
+        stopBtn.classList.add('d-none');
+        startBtn.classList.remove('d-none');
+    
+    })
